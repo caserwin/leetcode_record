ce523d1365e14575bf3a9ad5ccfac26af7056a4d
caserwin <zustxueyiding@163.com>
2021-10-07 23:18:01 +0800

    brackets

--- src/main/java/Code5_type1.java
+++ src/main/java/Code5_type1.java
@@ -43,12 +43,4 @@ public class Code5_type1 {
         }
         return true;
     }
-
-
-    private static String reverseString(String s) {
-        if (s.length() == 1) {
-            return s;
-        }
-        return s.substring(s.length() - 1) + reverseString(s.substring(0, s.length() - 1));
-    }
 }
--- src/main/java/special/brackets/Code20.java
+++ src/main/java/special/brackets/Code20.java
@@ -1,3 +1,5 @@
+package special.brackets;
+
 import java.util.HashMap;
 import java.util.Stack;
 

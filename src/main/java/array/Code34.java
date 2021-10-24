6e0490090c9337da9c80f290158b35e06532a49e
caserwin <zustxueyiding@163.com>
2021-10-24 10:47:10 +0800

    update

---
+++ src/main/java/array/Code34.java
@@ -0,0 +1,31 @@
+package array;
+
+/**
+ * @author yidxue
+ * 在排序数组中查找元素的第一个和最后一个位置
+ */
+public class Code34 {
+
+    /**
+     * O(n) 的复杂度
+     */
+    public int[] searchRange(int[] nums, int target) {
+        int first = -1;
+        int last = -1;
+
+        boolean is = true;
+        for (int i = 0; i < nums.length; i++) {
+            if (first != -1) {
+                is = false;
+            }
+
+            if (nums[i] == target) {
+                if (is) {
+                    first = i;
+                }
+                last = i;
+            }
+        }
+        return new int[]{first, last};
+    }
+}
